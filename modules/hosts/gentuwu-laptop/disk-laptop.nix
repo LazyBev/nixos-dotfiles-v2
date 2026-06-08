cd9014f96a2a508a615529090af037fd717c3d3b
LazyBev <lazy25yari@proton.me>
2026-06-08 14:23:55 +0100

    adding disko for laptop disks

--- modules/hosts/gentuwu-laptop/disk-laptop.nix
+++ modules/hosts/gentuwu-laptop/disk-laptop.nix
@@ -1,4 +1,6 @@
-{ config, lib, ... }: {
+{ inputs, config, lib, ... }: {
+  imports = [ inputs.disko.nixosModules.disko ];
+
   options.gentuwu.laptopHardware.enable = lib.mkEnableOption "gentuwu laptop hardware configuration";
 
   config = lib.mkIf config.gentuwu.laptopHardware.enable {
@@ -7,20 +9,46 @@
     boot.kernelModules = [ "kvm-intel" "rtw88_8822ce" ];
     boot.extraModulePackages = [ ];
 
-    fileSystems."/" = {
-      device = "/dev/disk/by-uuid/77b33934-7cdf-4cd8-b189-de6bc87b3ddf";
-      fsType = "ext4";
+    disko.devices.disk.nvme0n1 = {
+      type = "disk";
+      device = "/dev/nvme0n1";
+      content = {
+        type = "gpt";
+        partitions = {
+          boot = {
+            name = "EFI";
+            label = "EFI";
+            size = "1G";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "fmask=0077" "dmask=0077" ];
+            };
+          };
+          swap = {
+            name = "swap";
+            label = "swap";
+            size = "16G";
+            content = {
+              type = "swap";
+              discardPolicy = "both";
+            };
+          };
+          root = {
+            name = "root";
+            label = "root";
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+            };
+          };
+        };
+      };
     };
-
-    fileSystems."/boot" = {
-      device = "/dev/disk/by-uuid/06A4-51D3";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
-
-    swapDevices = [
-      { device = "/dev/disk/by-uuid/a0958f83-b9ab-44a1-a36a-1fde6ae655ac"; }
-    ];
 
     nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
     hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
