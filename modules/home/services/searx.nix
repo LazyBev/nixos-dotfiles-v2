0adb3cc5ef453cca3a71253c181492d479e58629
yari <yari@yari.xyz>
2026-06-08 03:08:03 +0100

    fixing searx

--- modules/home/services/searx.nix
+++ modules/home/services/searx.nix
@@ -1,8 +1,8 @@
 { inputs, pkgs, ... }: {
-  services.searxng = {
+  services.searx = {
     enable = true;
     settings = {
-      server.secret_key = "changeme";
+      server.secret_key = "idk";
       server.bind_address = "127.0.0.1";
       server.port = 8080;
     };
