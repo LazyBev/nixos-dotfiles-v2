754e3d5668567f0f8f8daf2fd2bde78007b3b12f
LazyBev <lazy25yari@proton.me>
2026-06-10 07:27:34 +0100

    Zellij config

--- modules/home/hm/xdg-config-files.nix
+++ modules/home/hm/xdg-config-files.nix
@@ -1,12 +1,13 @@
-{ ... }: {
-  xdg.configFile."dunst/dunstrc".source                         = ../../../configs/apps/dunst/dunstrc;
-  xdg.configFile."fuzzel/fuzzel.ini".source                     = ../../../configs/apps/fuzzel/fuzzel.ini;
-  xdg.configFile."yazi/theme.toml".source                       = ../../../configs/apps/yazi/theme.toml;
-  xdg.configFile."yazi/yazi.toml".source                        = ../../../configs/apps/yazi/yazi.toml;
-  xdg.configFile."yazi/keymap.toml".source                      = ../../../configs/apps/yazi/keymap.toml;
+{...}: {
+  xdg.configFile."dunst/dunstrc".source = ../../../configs/apps/dunst/dunstrc;
+  xdg.configFile."fuzzel/fuzzel.ini".source = ../../../configs/apps/fuzzel/fuzzel.ini;
+  xdg.configFile."yazi/theme.toml".source = ../../../configs/apps/yazi/theme.toml;
+  xdg.configFile."yazi/yazi.toml".source = ../../../configs/apps/yazi/yazi.toml;
+  xdg.configFile."yazi/keymap.toml".source = ../../../configs/apps/yazi/keymap.toml;
   xdg.configFile."yazi/flavors/dracula.yazi/flavor.toml".source = ../../../configs/apps/yazi/flavors/dracula.yazi/flavor.toml;
   xdg.configFile."yazi/flavors/dracula.yazi/tmtheme.xml".source = ../../../configs/apps/yazi/flavors/dracula.yazi/tmtheme.xml;
-  xdg.configFile."qutebrowser/config.py".source                 = ../../../configs/apps/qutebrowser/config.py;
-  xdg.configFile."gtklock/style.css".source                     = ../../../configs/apps/gtklock/style.css;
-  xdg.configFile."xdg-desktop-portal/portals.conf".text         = ''[preferred] default=gtk'';
+  xdg.configFile."qutebrowser/config.py".source = ../../../configs/apps/qutebrowser/config.py;
+  xdg.configFile."gtklock/style.css".source = ../../../configs/apps/gtklock/style.css;
+  xdg.configFile."xdg-desktop-portal/portals.conf".text = ''[preferred] default=gtk'';
+  xdg.configFile."zellij/config.kdl".source = ../../../configs/apps/zellij/config.kdl;
 }
