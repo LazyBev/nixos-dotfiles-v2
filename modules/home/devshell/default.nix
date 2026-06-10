15885b923f6799af60c5c8df1a9111e1f0860802
LazyBev <lazy25yari@proton.me>
2026-06-10 20:39:56 +0000

    dev-shell env

--- modules/home/devshell/default.nix
+++ modules/home/devshell/default.nix
@@ -11,5 +11,30 @@
         nh
       ];
     };
+
+    devShells.orix-dev = pkgs.mkShell {
+      packages = with pkgs; [
+        ocaml
+        ocamlPackages.findlib
+        gcc
+        gnumake
+        curl
+        cpio
+        gzip
+        grub2
+        libisoburn
+        file
+        patchelf
+        flex
+        bison
+        bc
+        libxcrypt
+        qemu
+        parted
+        e2fsprogs
+        util-linux
+        openssl
+      ];
+    };
   };
 }
